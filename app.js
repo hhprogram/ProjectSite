2645a102a0705567d613095c1a08315127a986a4
hhprogram <li.harrison.h@gmail.com>
2018-04-03 17:08:09 +0000

    adding whalegame page as well as some more helpful comments

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ var express = require("express");
             
 
 var app = express();
-// looks in the current directory where app.js lives ('./')
-var indexRoutes = require("./routes/index")
+// looks in the current directory where app.js lives ('./') and then looks in the 'routes' directory
+// also note that we don't have to notify specifically to require /routes/index.js because by 
+// default express will automatically find and required the index.js file within the 'routes' directory
+// if specific file not given
+var indexRoutes = require("./routes")
+var whaleRoutes = require("./routes/Whale")
 
 
 // tells express that the templates are all ejs so you don't have to add .ejs to all templates
@@ -17,8 +21,11 @@ app.get("/", function(req, res) {
 })
 
 // this tells express to use variable INDEXROUTES whenever the url "/" is pinged, therefore every route
-// within the index.js file has "/" appended to it
+// within the index.js file has "/" prepended to it
 app.use("/", indexRoutes)
+// this tells express to use variable INDEXROUTES whenever the url "/" is pinged, therefore every route
+// within the index.js file has "/WhaleGame" prepended to it
+app.use("/WhaleGame", whaleRoutes)
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The project site server Has Started!");
---
+++ routes/Whale/index.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var router = express.Router();
+
+// note: I don't have to put "/WhaleGame" in front of each of these URLs. because 
+// in my app.js file. I have told Express that everything in this file
+// should have the path /WhaleGame appended to it
+
+router.get("/", function(req, res) {
+    res.render("Whale/home")
+});
+
+// note: need to remember to do this or else will get an error. For eaxxmple the use()
+// function in app.js that uses this file will throw an error
+module.exports = router
